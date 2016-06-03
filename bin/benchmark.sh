d2537bf42027c71feb2805fedb98a40c85755d8d
Aneesh Neelam <neelam.aneesh@gmail.com>
2016-06-02 22:05:20 -0700

    Added benchmark script
    * Script to run dd and bonnie++
    * Initial Bonnie++ results

---
+++ bin/benchmark.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+NATIVE_BENCHMARK_DIR="/Users/aneeshneelam/benchmarks"
+DIFUSE_BENCHMARK_DIR="/Users/aneeshneelam/Temp/Users/aneeshneelam/benchmarks"
+
+
+for ((n=0;n<20;n++));
+do
+  echo "Native Write"
+  time sh -c "dd if=/dev/zero of=${NATIVE_BENCHMARK_DIR}/testfile bs=100k count=1k && sync";
+done
+
+for ((n=0;n<20;n++));
+do
+  echo "Native Read"
+  time sh -c "dd of=/dev/null if=${NATIVE_BENCHMARK_DIR}/testfile bs=100k count=1k && sync";
+done
+
+for ((n=0;n<20;n++));
+do
+  echo "DiFUSE Write"
+  time sh -c "dd if=/dev/zero of=${DIFUSE_BENCHMARK_DIR}/testfile bs=100k count=1k && sync";
+done
+
+for ((n=0;n<20;n++));
+do
+  echo "DiFUSE Read"
+  time sh -c "dd of=/dev/null if=${DIFUSE_BENCHMARK_DIR}/testfile bs=100k count=1k && sync";
+done
+
+rm ${NATIVE_BENCHMARK_DIR}/testfile
+
+
+for ((n=0;n<10;n++));
+do
+echo "Native Bonnie++"
+   /usr/local/Cellar/bonnie++/1.97/sbin/bonnie++ -r 3072 -d ${NATIVE_BENCHMARK_DIR}
+done
+
+for ((n=0;n<10;n++));
+do
+echo "DiFUSE Bonnie++"
+   /usr/local/Cellar/bonnie++/1.97/sbin/bonnie++ -r 3072 -d ${DIFUSE_BENCHMARK_DIR}
+done
